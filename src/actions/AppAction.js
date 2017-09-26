25ad68d402c91201d15091c60b5d38ebe8bb87cd
Tim Tawan <timtawans@gmail.com>
2017-09-26 17:18:23 +1000

    added example

--- src/actions/AppAction.js
+++ src/actions/AppAction.js
@@ -1,3 +1,9 @@
 import { createAction } from 'redux-actions'
 
-export const sayHello = createAction('SAY_HELLO');
+export const sayHello = createAction('SAY_HELLO')
+
+// Example of action
+// const { increment, decrement } = createActions({
+//   'INCREMENT': amount => ({ amount: 1 }),
+//   'DECREMENT': amount => ({ amount: -1 })
+// })
